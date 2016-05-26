d45a92c6d6d6ff1ac77299921363cb5e49bf51c9
Tom Adams <tomjadams@gmail.com>
2016-05-26 10:31:07 +1000

    Comments on usage

--- src/main/scala/finchtemplate/util/hawk/HawkAuthenticate.scala
+++ src/main/scala/finchtemplate/util/hawk/HawkAuthenticate.scala
@@ -4,9 +4,15 @@ import finchtemplate.util.hawk.params.{KeyData, RequestContext}
 import finchtemplate.util.hawk.validate.HeaderValidator.validateHeader
 import finchtemplate.util.hawk.validate.PayloadValidator.validatePayload
 
-/*
- * Authenticate an incoming request using Hawk.
- */
+/**
+  * Authenticate an incoming request using Hawk.
+  *
+  * Performs header validation [1] by default, but will also do payload validation [2] if a `{{{Some(payload)}}} is
+  * provided in the {{{RequestContext}}}.
+  *
+  * [1] https://github.com/hueniverse/hawk#protocol-example
+  * [2] https://github.com/hueniverse/hawk#payload-validation
+  **/
 object HawkAuthenticate {
   def authenticate(key: KeyData, context: RequestContext): Boolean = {
     val headerValid = validateHeader(key, context.header)
