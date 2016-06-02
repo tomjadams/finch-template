146a96d4aea35c29b2d0263b3f08fa751e261587
Tom Adams <tomjadams@gmail.com>
2016-06-02 20:54:17 +1000

    Auth filter compiles

--- src/main/scala/finchtemplate/auth/AuthenticationFilter.scala
+++ src/main/scala/finchtemplate/auth/AuthenticationFilter.scala
@@ -1,59 +1,21 @@
 package finchtemplate.auth
 
-import java.net.URI
-
 import cats.data.Xor
-import com.github.benhutchison.mouse.all._
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.{Filter, Service}
-import com.twitter.io.Buf
-import com.twitter.util.{Future, Try}
-import finchtemplate.util.hawk.HawkAuthenticate.parseRawRequestAuthHeader
-import finchtemplate.util.hawk.TaggedTypesFunctions.RawAuthenticationHeader
+import com.twitter.util.Future
+import finchtemplate.auth.RequestContextBuilder.buildContext
+import finchtemplate.util.hawk.HawkAuthenticate._
 import finchtemplate.util.hawk._
-import finchtemplate.util.hawk.params.HttpMethod.httpMethod
-import finchtemplate.util.hawk.params._
-import finchtemplate.util.hawk.validate.{Credentials, RequestAuthorisationHeader}
+import finchtemplate.util.hawk.validate.Credentials
 
 final class AuthenticationFilter(credentials: Credentials) extends Filter[Request, Response, Request, Response] {
   override def apply(request: Request, service: Service[Request, Response]): Future[Response] =
-    authenticateRequest(request).fold(e => Future.exception(e), _ => service(request))
+    authenticate(request).fold(e => Future.exception(e), _ => service(request))
 
-  private def authenticateRequest(request: Request): Xor[Error, RequestValid] = {
-
-    for {
-      header <- parseAuthHeader(request)
-      method <- httpMethod(request.method.toString())
-      requestUri <- Try(new URI(request.uri)).toOption
-      host = request.host.map(Host(_)).getOrElse(Host.UnknownHost)
-      port = Port(requestUri.getPort)
-      path = UriPath(requestUri.getRawPath)
-
-    } yield {
-      val pc = methodDependantPayloadContext(method, request.contentType, request.content)
-      val rc = new RequestContext(method, host, port, path, header, pc)
-      HawkAuthenticate.authenticateRequest(credentials, rc)
-    }
-
-  }
-
-
-  private def methodDependantPayloadContext(method: HttpMethod, contentType: Option[String], content: Buf): Option[PayloadContext] =
-    List(Put, Post, Patch).contains(method).option(payloadContext(contentType, content))
-
-  private def payloadContext(contentType: Option[String], content: Buf): PayloadContext = {
-    val ct = contentType.map(ContentType(_)).getOrElse(ContentType.UnknownContentType)
-    PayloadContext(ct, bufToStream(content))
-  }
-
-  private def parseAuthHeader(request: Request): Option[RequestAuthorisationHeader] =
-    request.headerMap.get(AuthorisationHttpHeader).flatMap(s => parseRawRequestAuthHeader(RawAuthenticationHeader(s)))
-
-
-  private def bufToStream(b: Buf): Array[Byte] = {
-    val output = new Array[Byte](b.length)
-    b.write(output, 0)
-    output
+  private def authenticate(request: Request): Xor[Error, RequestValid] = {
+    val valid: Option[Xor[Error, RequestValid]] = buildContext(request).map(context => authenticateRequest(credentials, context))
+    valid.getOrElse(errorXor("bzzzt"))
   }
 }
 
---
+++ src/main/scala/finchtemplate/auth/RequestContextBuilder.scala
@@ -0,0 +1,47 @@
+package finchtemplate.auth
+
+import java.net.URI
+
+import com.github.benhutchison.mouse.all._
+import com.twitter.finagle.http.Request
+import com.twitter.io.Buf
+import com.twitter.util.Try
+import finchtemplate.util.hawk.HawkAuthenticate._
+import finchtemplate.util.hawk.TaggedTypesFunctions._
+import finchtemplate.util.hawk._
+import finchtemplate.util.hawk.params.HttpMethod._
+import finchtemplate.util.hawk.params._
+import finchtemplate.util.hawk.validate.RequestAuthorisationHeader
+
+object RequestContextBuilder {
+  // TODO TJA THis should return Xor[Error, RequestContext] so we get the error out
+  def buildContext(request: Request): Option[RequestContext] =
+    for {
+      header <- parseAuthHeader(request)
+      method <- httpMethod(request.method.toString())
+      requestUri <- Try(new URI(request.uri)).toOption
+    } yield {
+      val host = request.host.map(Host(_)).getOrElse(Host.UnknownHost)
+      val port = Port(requestUri.getPort)
+      val path = UriPath(requestUri.getRawPath)
+      val pc = methodDependantPayloadContext(method, request.contentType, request.content)
+      new RequestContext(method, host, port, path, header, pc)
+    }
+
+  private def parseAuthHeader(request: Request): Option[RequestAuthorisationHeader] =
+    request.headerMap.get(AuthorisationHttpHeader).flatMap(s => parseRawRequestAuthHeader(RawAuthenticationHeader(s)))
+
+  private def methodDependantPayloadContext(method: HttpMethod, contentType: Option[String], content: Buf): Option[PayloadContext] =
+    List(Put, Post, Patch).contains(method).option(payloadContext(contentType, content))
+
+  private def payloadContext(contentType: Option[String], content: Buf): PayloadContext = {
+    val ct = contentType.map(ContentType(_)).getOrElse(ContentType.UnknownContentType)
+    PayloadContext(ct, bufToStream(content))
+  }
+
+  private def bufToStream(b: Buf): Array[Byte] = {
+    val output = new Array[Byte](b.length)
+    b.write(output, 0)
+    output
+  }
+}
